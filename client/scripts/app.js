a1a0012df0539160245588a33d4a4896afcb92c6
rhinodavid <dawalsh@gmail.com>
2016-09-26 15:48:59 -0700

    Implement initial methods

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,56 @@
 // YOUR CODE HERE:
+
+
+var app = {
+  server: 'https://api.parse.com/1/classes/messages'
+};
+
+app.init = function() {
+
+};
+
+app.send = function(message) {
+  $.ajax({
+    url: this.server,
+    type: 'POST',
+    data: JSON.stringify(message), 
+    success: (data, textStatus, jqXHR) => {
+      console.log(data);
+      console.log(textStatus);
+      console.log(jqXHR);
+    }
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    url: this.server, 
+    success: (data, textStatus, jqXHR) => {
+      console.log(data);
+      console.log(textStatus);
+      console.log(jqXHR);
+    }
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+
+app.renderMessage = function(message) {
+  let $message = $('<div></div>').append('<span class="username">' + message.username + '</span>');
+  $message.text(message.text);
+  $('#chats').append($message);
+};
+
+app.renderRoom = function(roomName) {
+  let $newRoom = $('<li></li>').text(roomName);
+  $('#roomSelect').append($newRoom);
+};
+
+app.handleUsernameClick = function(username) {
+  // add friend?
+};
+
+
+
