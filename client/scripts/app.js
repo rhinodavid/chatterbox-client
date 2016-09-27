7b321719fef4c21b34d2a17bd9d1cc371f3b38ed
rhinodavid <dawalsh@gmail.com>
2016-09-26 20:47:24 -0700

    Implement room creation and switching

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -6,10 +6,17 @@ var app = {
 };
 
 app.init = function() {
-  $('#send').off('submit').submit(app.handleSubmit);
+  $('#send').submit(app.handleSubmit);
+  $('#roomSelect').on('change', app.handleRoomChange);
+
+  // add New Room option to rooms
+  let $newRoom = $('<option value="add_new_room"></option>').text('Add new room...');
+  $('#roomSelect').append($newRoom);
+
   app.currentRoom = 'lobby';
   app.rooms = [].concat(app.currentRoom);
   app.renderRoom(app.currentRoom);
+  document.querySelector('#roomSelect [value="lobby"]').selected = true;
   app.fetch();
 };
 
@@ -60,20 +67,20 @@ app.clearMessages = function() {
 };
 
 app.renderMessage = function(message) {
-  let messageText = $('<div>').text(message.text).html();
-  let userName = $('<div>').text(message.username).html();
+  const messageText = $('<div>').text(message.text).html();
+  const userName = $('<div>').text(message.username).html();
 
 
-  let $message = $('<div></div>').addClass('message');
-  let $username = $('<span></span>').addClass('message-username').text(userName);
-  let $text = $('<span></span>').addClass('message-text').text(messageText);
+  const $message = $('<div></div>').addClass('message');
+  const $username = $('<span></span>').addClass('username').text(userName);
+  const $text = $('<span></span>').addClass('text').text(messageText);
   $message.append($username).append($text);
   $('#chats').append($message);
 };
 
 app.renderRoom = function(roomName) {
-  let roomNameEscaped = $('<div>').text(roomName).html();
-  let $newRoom = $('<li></li>').text(roomNameEscaped);
+  const roomNameEscaped = $('<div>').text(roomName).html();
+  const $newRoom = $('<option value="' + roomNameEscaped + '"></option>').text(roomNameEscaped);
   $('#roomSelect').append($newRoom);
 };
 
@@ -81,13 +88,24 @@ app.handleUsernameClick = function(username) {
   // add friend?
 };
 
+app.handleRoomChange = function(event) {
+  // see if it's the add new room value
+  if (event.target.value === 'add_new_room') {
+    app.createNewRoom();
+  } else {
+    // reset the current room
+    app.currentRoom = event.target.value;
+    app.fetch();
+  }
+};
+
 app.handleSubmit = function(event) {
   event.preventDefault();
   // grab message from form
-  let messageText = $('#message').val();
+  const messageText = $('#message').val();
   $('#message').val('');
-  let messageUsername = app.getUsername();
-  let messageRoomname = app.room;
+  const messageUsername = app.getUsername();
+  const messageRoomname = app.currentRoom;
 
   // message has 'text', 'username', and 'roomname'
 
@@ -98,6 +116,25 @@ app.handleSubmit = function(event) {
     username: messageUsername,
     roomname: messageRoomname
   });
+};
+
+app.createNewRoom = function() {
+  const name = prompt('Enter new room name.') || 'lobby';
+  // see if it's in out list at all
+  if (app.rooms.indexOf(name) > -1) {
+    // if it is, just change the room to the input room
+    app.currentRoom = name;
+  } else {
+    // if it is not, then add the room, change current room, and change the
+    // displayed room
+    app.rooms.push(name);
+    app.currentRoom = name;
+    // add it to screen
+    app.renderRoom(name);
+  }
+  // fetch and update the displayed room
+  app.fetch();
+  document.querySelector('#roomSelect [value="' + name + '"]').selected = true;
 };
 
 app.getUsername = function() {
